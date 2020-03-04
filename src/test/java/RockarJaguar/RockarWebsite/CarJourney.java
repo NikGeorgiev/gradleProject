09f9fb3566bda986996789e6e17c34edb30e06e0
Nikolay Georgiev <nikolaygeorgiev@Nikolays-MacBook-Pro.local>
2020-03-04 11:42:51 +0000

    improved css selectors

--- src/test/java/RockarJaguar/RockarWebsite/CarJourney.java
+++ src/test/java/RockarJaguar/RockarWebsite/CarJourney.java
@@ -9,11 +9,11 @@ public class CarJourney {
     private WebDriver driver;
     private String homePageURL = "https://jaguar.rockar.com/";
     private By FindNewCarBtn = By.cssSelector("ul.navigation li");
-    private By NextStepBtn = By.cssSelector("#car-finder > div.navigation-buttons-wrapper.col-12 > div.navigation-outer-container > button:nth-child(2)");
-    private By ContinueBtn = By.cssSelector("#finance-filter > div.popup.finance-confirmation-popup > div.popup-container > div > div > div > div > div.row.align-right > button.button.button-narrow.popup-continue");
-    private By SkipThisStepBtn = By.cssSelector("#car-finder > div.car-finder-filters > div.part-exchange > div.substep-1 > div.row > div > div.part-exchange-right > div > button");
+    private By NextStepBtn = By.cssSelector(".button-narrow.next-step");
+    private By ContinueBtn = By.cssSelector(".button-narrow.popup-continue");
+    private By SkipThisStepBtn = By.cssSelector(".button.skip-button");
     private By SelectXfModel = By.cssSelector(".slick-center img");
-    private By ContinueAfterTickbox = By.cssSelector("#car-finder > div.navigation-buttons-wrapper.col-12 > div.navigation-outer-container > button.button.button-narrow.next-step.continue.desktop-tablet-only");
+    private By ContinueAfterTickbox = By.cssSelector(".button.button-narrow.next-step.continue");
     public CarJourney (WebDriver driver){
 
         this.driver = driver;
