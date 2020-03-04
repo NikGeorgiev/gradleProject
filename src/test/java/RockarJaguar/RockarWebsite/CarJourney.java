d2795fec6b1a04e4a1041b1f291da4c11fa0fa0a
Anis Subba <anissubba@Rockar-Laptop-15MBPs-MacBook-Pro.local>
2020-03-04 12:31:02 +0000

    no .gitignore bro

--- src/test/java/RockarJaguar/RockarWebsite/CarJourney.java
+++ src/test/java/RockarJaguar/RockarWebsite/CarJourney.java
@@ -2,7 +2,9 @@ package RockarJaguar.RockarWebsite;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CarJourney {
@@ -14,25 +16,22 @@ public class CarJourney {
     private By SkipThisStepBtn = By.cssSelector(".button.skip-button");
     private By SelectXfModel = By.cssSelector(".slick-center img");
     private By ContinueAfterTickbox = By.cssSelector(".button.button-narrow.next-step.continue");
-    public CarJourney (WebDriver driver){
+    public String carPriceSelector = ".rockar-price .price";
 
+    public CarJourney (WebDriver driver){
         this.driver = driver;
     }
-
-
 
     public CarJourney goToHomePage(){
         waitForElement();
         driver.navigate().to(homePageURL);
         return this;
-
     }
 
     public CarJourney goToFindNewCarBtn(){
         waitForElement();
         driver.findElement(FindNewCarBtn).click();
         return this;
-
     }
 
     public CarJourney clickNextStep(){
@@ -40,35 +39,61 @@ public class CarJourney {
         driver.findElement(NextStepBtn).click();
         return this;
     }
+
     public CarJourney waitForElement() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return this;
     }
+
     public CarJourney pressContinue(){
         driver.findElement(ContinueBtn).click();
         return this;
-
     }
+
     public CarJourney skipThisStep(){
         driver.findElement(SkipThisStepBtn).click();
         return this;
     }
+
     public CarJourney selectXF(){
         //Sleep for testing purposes
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         driver.findElement(SelectXfModel).click();
         return this;
     }
+
     public CarJourney continueAfterTickBox(){
         driver.findElement(ContinueAfterTickbox).click();
         return this;
     }
 
-
-
-
+    public CarJourney getPrice (){
+        //sleep for testing purposes
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // list all the cars in the page and prints it out
+        List<WebElement> carPriceList = driver.findElements(By.cssSelector(carPriceSelector));
+        carPriceList.forEach((i) -> System.out.println((i.getText().replaceAll("[£,]", ""))));
+        //had to sleep as it was too fast
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Calculates the total Price of the cars avaliable
+        int totalPriceOfAvailableCars = 0;
+        for (WebElement a : carPriceList){
+            totalPriceOfAvailableCars += Integer.parseInt(a.getText().replaceAll("[£,]", ""));
+        }
+        System.out.println("Total Prices of Cars: " + totalPriceOfAvailableCars);
+        driver.close();
+        return this;
+    }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -22,32 +22,12 @@ public class Tests {
     private static SeleniumConfig seleniumConfig;
     private static CarJourney carJourney;
 
-    //public WebDriver driver = new ChromeDriver();
-
-
     @BeforeClass
     public static void coverJourney() {
-        seleniumConfig = new SeleniumConfig("chrome","/Users/nikolaygeorgiev/Desktop/gradleProject/chromedriver");
+        seleniumConfig = new SeleniumConfig("chrome","/Users/anissubba/Documents/gradleProject/chromedriver");
         carJourney = new CarJourney(seleniumConfig.getDriver());
         }
-//        CarJourney carJourney = new CarJourney();
-//    }
-    /*1Do this
-    Get color
-    2Then this
-    get model
-    3then this
-    Get the price of the car
-    get the title of the car
-    4then this
-    Get bal bla
-    5then this
-    get bla
-    1 Check if color is pink
-    2 Check if model is new
 
-
-            */
     @Test
     public void PhaseOne(){
         carJourney.goToHomePage()
@@ -56,50 +36,7 @@ public class Tests {
                 .pressContinue()
                 .skipThisStep()
                 .selectXF()
-                .continueAfterTickBox();
+                .continueAfterTickBox()
+                .getPrice();
     }
-
-//    @Test
-//    public void PhaseTwo(){
-//        carJourney.goToFindNewCarBtn();
-//    }
-//    @Test
-//    public void PhaseThree(){
-//        carJourney.clickNextStep();
-//    }
-
-//    @Test
-//    public void DemoMitsubishiCarsTotal(){
-//        System.setProperty("webdriver.chrome.drive", "chromedriver");
-//        driver.get("https://www.mitsubishi-motors.co.uk/cars/eclipse-cross");
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        List<WebElement> price = driver.findElements(By.cssSelector("#selector-2-tab-4 > div > a > div > p > span"));
-//        price.forEach((i) -> System.out.println(i.getText().replaceAll("[£,]","")));
-//        int total = 0;
-//        for (WebElement a : price){
-//            total += Integer.parseInt(a.getText().replaceAll("[£,]",""));
-//        }
-//        System.out.println("Total: " + total);
-//        driver.close();
-//    }
-
-//    @Test
-//    public void DemoAutoTradeCarsTotal(){
-//        System.setProperty("webdriver.chrome.drive", "chromedriver");
-//        driver.get("https://www.autotrader.co.uk/car-search?advertClassification=standard&postcode=HU6%207JH&on" +
-//                "esearchad=Used&onesearchad=Nearly%20New&onesearchad=New&advertising-location=at_cars&is-quick-search=TRUE&page=1");
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        List<WebElement> price = driver.findElements(By.cssSelector(".search-page__result .vehicle-price"));
-//        price.forEach((i) -> System.out.println(i.getText().replaceAll("[£,]","")));
-//        int total = 0;
-//        for (WebElement b : price){
-//            total += Integer.parseInt(b.getText().replaceAll("[£,]", ""));
-//        }
-//        System.out.println("Total: " + total);
-//        driver.close();
-//    }
-
-
-
-
 }
